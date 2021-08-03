b59cc5bd808a43fcf06df90f1f91939e2a79710d
Volodymyr Voronov <malones.vladimir@gmail.com>
2021-08-03 20:44:10 +0200

    chore(helpers): created conts with regex

--- src/helpers/checkInputsSigninFormValidity.ts
+++ src/helpers/checkInputsSigninFormValidity.ts
@@ -6,6 +6,9 @@ type FormData = {
   password2?: string;
 };
 
+const regexLogin = /^[A-Za-z0-9]*$/;
+const regexPassword = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8,})$/;
+
 export const checkInputsSigninFormValidity = (formData: FormData) => {
   const errors: { [key: string]: string } = {};
 
@@ -23,13 +26,13 @@ export const checkInputsSigninFormValidity = (formData: FormData) => {
 
   if (!formData.login) {
     errors.login = "Логин не должен быть пустым.";
-  } else if (!formData.login.match(/^[A-Za-z0-9]*$/)) {
-    errors.login = "Логин должен содержать только латинские буквы.";
+  } else if (!formData.login.match(regexLogin)) {
+    errors.login = "Только латинские буквы.";
   }
 
   if (!formData.password) {
     errors.password = "Пароль не должен быть пустым.";
-  } else if (!formData.password.match(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8,})$/)) {
+  } else if (!formData.password.match(regexPassword)) {
     errors.password = "Латинскиe буквы, цифры и знаки.";
   } else if (formData.password !== formData.password2) {
     errors.password = "Пароли должны совпадать.";
@@ -37,7 +40,7 @@ export const checkInputsSigninFormValidity = (formData: FormData) => {
 
   if (!formData.password2) {
     errors.password2 = "Пароль не должен быть пустым.";
-  } else if (!formData.password2.match(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8,})$/)) {
+  } else if (!formData.password2.match(regexPassword)) {
     errors.password2 = "Латинскиe буквы, цифры и знаки.";
   } else if (formData.password !== formData.password2) {
     errors.password2 = "Пароли должны совпадать.";
